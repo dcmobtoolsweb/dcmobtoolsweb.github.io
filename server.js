0640e9ae92cf3fd4f5730e5986e10fc861e74ae0
Jin Zhang <jin_zhang@thinkyeah.com>
2023-03-15 15:08:35 +0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.get('host')}${req.url}`);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public', { extensions: ['html'] }));
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`server started ${port}`);
